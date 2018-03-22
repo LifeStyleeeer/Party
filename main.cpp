b546e3f010be2ff9279a6d23ba40fb2662d3ffd0
Stanislav Ivailov <36530763+LifeStyleeeer@users.noreply.github.com>
2018-03-22 11:24:23 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+enum Colour {black,white};
+
+{
+class BattleShip
+    {
+        private:
+    long id;
+    string name;
+    Colour clr;
+    int attackDmg;
+    int life;
+    int shield;
+    int capacity;
+
+};
+class Outrider:public BattleShip
+{
+
+};
+int main()
+{
+
+}
+    return 0;
+
+}
